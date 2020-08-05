926893c04c5bbb4c5ce47025786f283b858abc62
rumbie <rumbiemudzie@gmail.com>
2020-08-05 18:57:04 +0200

    Romove the last two tests

--- spec/telegram_spec.rb
+++ spec/telegram_spec.rb
@@ -1,32 +1,32 @@
 require_relative '../lib/telegram_bot.rb'
 require_relative '../lib/quotes.rb'
 
-# RSpec.describe do
-#   it 'should inherit behavior from Parent' do
-#     expect(Bot < Inspire).to eq true
-#   end
+RSpec.describe do
+  it 'should inherit behavior from Parent' do
+    expect(Bot < Inspire).to eq true
+  end
 
-#   it 'is not inherited' do
-#     expect(Bot < Inspire).!= eq true
+  it 'is not inherited' do
+    expect(Bot < Inspire).!= eq true
+  end
+end
+
+# describe Inspire do
+#   describe '#request_joke' do
+#     it 'returns a joke from the API url' do
+#       joke = Inspire.new
+#       allow(joke).to receive(:request_joke).and_return('Why do choirs keep buckets handy? So they can carry their tune')
+#       expect(joke.request_joke).to eq('Why do choirs keep buckets handy? So they can carry their tune')
+#     end
 #   end
 # end
 
-describe Inspire do
-  describe '#request_joke' do
-    it 'returns a joke from the API url' do
-      joke = Inspire.new
-      allow(joke).to receive(:request_joke).and_return('Why do choirs keep buckets handy? So they can carry their tune')
-      expect(joke.request_joke).to eq('Why do choirs keep buckets handy? So they can carry their tune')
-    end
-  end
-end
-
-describe Inspire do
-  describe '#request_joke' do
-    it 'returns an inspiration from the API url' do
-      inspiration = Inspire.new
-      allow(inspiration).to receive(:request_inspiration).and_return('An Apple a day keeps you busy anyway')
-      expect(inspiration.request_inspiration).to eq('An Apple a day keeps you busy anyway')
-    end
-  end
-end
+# describe Inspire do
+#   describe '#request_joke' do
+#     it 'returns an inspiration from the API url' do
+#       inspiration = Inspire.new
+#       allow(inspiration).to receive(:request_inspiration).and_return('An Apple a day keeps you busy anyway')
+#       expect(inspiration.request_inspiration).to eq('An Apple a day keeps you busy anyway')
+#     end
+#   end
+# end
