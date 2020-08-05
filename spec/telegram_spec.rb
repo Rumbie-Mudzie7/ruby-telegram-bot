d0c06c5018f6e2462c305b6a25d17300a8ff6363
rumbie <rumbiemudzie@gmail.com>
2020-08-05 19:57:53 +0200

    Fix linter errors

--- spec/telegram_spec.rb
+++ spec/telegram_spec.rb
@@ -11,22 +11,22 @@ RSpec.describe do
   end
 end
 
-# describe Inspire do
-#   describe '#request_joke' do
-#     it 'returns a joke from the API url' do
-#       joke = Inspire.new
-#       allow(joke).to receive(:request_joke).and_return('Why do choirs keep buckets handy? So they can carry their tune')
-#       expect(joke.request_joke).to eq('Why do choirs keep buckets handy? So they can carry their tune')
-#     end
-#   end
-# end
+describe Inspire do
+  describe '#request_joke' do
+    it 'returns a joke from the API url' do
+      joke = Inspire.new
+      allow(joke).to receive(:request_joke).and_return('Why do choirs keep buckets handy? So they can carry their tune')
+      expect(joke.request_joke).to eq('Why do choirs keep buckets handy? So they can carry their tune')
+    end
+  end
+end
 
-# describe Inspire do
-#   describe '#request_joke' do
-#     it 'returns an inspiration from the API url' do
-#       inspiration = Inspire.new
-#       allow(inspiration).to receive(:request_inspiration).and_return('An Apple a day keeps you busy anyway')
-#       expect(inspiration.request_inspiration).to eq('An Apple a day keeps you busy anyway')
-#     end
-#   end
-# end
+describe Inspire do
+  describe '#request_joke' do
+    it 'returns an inspiration from the API url' do
+      inspiration = Inspire.new
+      allow(inspiration).to receive(:request_inspiration).and_return('An Apple a day keeps you busy anyway')
+      expect(inspiration.request_inspiration).to eq('An Apple a day keeps you busy anyway')
+    end
+  end
+end
