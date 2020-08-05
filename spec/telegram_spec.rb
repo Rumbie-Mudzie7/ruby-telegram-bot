55cb2de4cee0c3d0d2b39a2ceea7e2e60197cbc2
rumbie <rumbiemudzie@gmail.com>
2020-08-05 18:54:10 +0200

    Remove tests cases

--- spec/telegram_spec.rb
+++ spec/telegram_spec.rb
@@ -1,15 +1,15 @@
 require_relative '../lib/telegram_bot.rb'
 require_relative '../lib/quotes.rb'
 
-RSpec.describe do
-  it 'should inherit behavior from Parent' do
-    expect(Bot < Inspire).to eq true
-  end
+# RSpec.describe do
+#   it 'should inherit behavior from Parent' do
+#     expect(Bot < Inspire).to eq true
+#   end
 
-  it 'is not inherited' do
-    expect(Bot < Inspire).!= eq true
-  end
-end
+#   it 'is not inherited' do
+#     expect(Bot < Inspire).!= eq true
+#   end
+# end
 
 describe Inspire do
   describe '#request_joke' do
